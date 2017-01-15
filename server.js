b32ee742f7e4f48b80af54d9e229a52dda0dfbca
Michael Cabelin <ncabelin@gmail.com>
2017-01-15 01:18:47 -0800

    add components

--- server.js
+++ server.js
@@ -2,14 +2,36 @@
 
 var express = require('express'),
 		app = express(),
-		secret = require('secret_keys'),
+		secret = require('./secret_keys'),
 		Yelp = require('yelp');
 
 var yelp = new Yelp({
-	consumer_key: secret.key('c_k'),
-	consumer_secret: secret.key('c_s'),
-	token: secret.key('token'),
-	token_secret: secret.key('token_s')
+	consumer_key: secret('c_k'),
+	consumer_secret: secret('c_s'),
+	token: secret('token'),
+	token_secret: secret('token_s')
 });
 
-console.log('done');
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.get('/:searchterm', function(req, res) {
+	var searchterm = req.params.searchterm;
+	yelp.search({
+		term: searchterm,
+		location: 'Los Angeles'})
+	.then(function(data) {
+		res.json(data);
+	}).catch(function(err) {
+		res.status(400).send(err);
+	});
+});
+
+app.listen(process.env.PORT || 8080, function() {
+	console.log('started server...');
+})
