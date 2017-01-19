f3d6d66242392c0e1e461db73c688971358fc0af
Michael Cabelin <ncabelin@gmail.com>
2017-01-19 00:15:49 -0800

    render app in node express server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express'),
 		app = express(),
 		secret = require('./secret_keys'),
 		path = require('path'),
+		request = require('request'),
 		Yelp = require('yelp');
 
 var yelp = new Yelp({
@@ -24,7 +25,17 @@ app.use(function (req, res, next) {
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname+'/index.html'));
+	var fTemp = '';
+	request('http://api.openweathermap.org/data/2.5/weather?q=Los Angeles,CA&appid=f6528aa612e42b74b4f7bcf00cd1b0b1', function(err, response, body) {
+		if (!err && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			var temp = (1.8 * (data.main.temp - 273)) + 32; // convert Kelvin to Fahrenheit
+      fTemp = '<span><h3><strong>' + Math.round(temp) + '&deg;</strong> f</h3> right now (by <a href="http://openweathermap.org">OpenWeather)</a></span>';
+      res.render('index.ejs', { fTemp: fTemp });
+		} else {
+			res.status(400).send('Error getting weather');
+		}
+	});
 });
 
 app.get('/:searchterm', function(req, res) {
@@ -38,6 +49,8 @@ app.get('/:searchterm', function(req, res) {
 		res.status(400).send(err);
 	});
 });
+
+app.get('/weather')
 
 app.listen(process.env.PORT || 8080, function() {
 	console.log('started server...');
